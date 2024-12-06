c3dff83d48736b47403b3858eae055e68ade0925
William Brown <wsbrown.colo@gmail.com>
2024-12-06 16:57:22 -0700

    User story #1

--- src/com/skilldistillery/filmquery/app/FilmQueryApp.java
+++ src/com/skilldistillery/filmquery/app/FilmQueryApp.java
@@ -1,5 +1,6 @@
 package com.skilldistillery.filmquery.app;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import com.skilldistillery.filmquery.database.DatabaseAccessor;
@@ -7,36 +8,54 @@ import com.skilldistillery.filmquery.database.DatabaseAccessorObject;
 import com.skilldistillery.filmquery.entities.Film;
 
 public class FilmQueryApp {
-  
-  DatabaseAccessor db = new DatabaseAccessorObject();
 
-  public static void main(String[] args) {
-    FilmQueryApp app = new FilmQueryApp();
-    app.test();
-    //app.launch();
-  }
+	DatabaseAccessor db = new DatabaseAccessorObject();
+	Scanner input = new Scanner(System.in);
 
-  private void test() {
-    Film film = db.findFilmById(1);
-    System.out.println(film);
-    System.out.println(film.getActors());
-  }
+	public static void main(String[] args) {
+		FilmQueryApp app = new FilmQueryApp();
+//    app.test();
+		app.launch();
+	}
+//  private void test() {
+//    Film film = db.findFilmById(1);
+//    System.out.println(film);
+//    System.out.println(film.getActors());
+//  }
 
-  
-  
-  private void launch() {
-    Scanner input = new Scanner(System.in);
-    
-    startUserInterface(input);
-    
-    input.close();
-  }
+	private void launch() {
+		startUserInterface(input);
 
-  private void startUserInterface(Scanner input) {
-    //TODO Print menu
-	  //TODO Get choice
-	  //TODO Act on choice
-	  //TODO repeat until quit
-  }
+		input.close();
+	}
+
+	private void startUserInterface(Scanner input) {
+		// TODO Print menu
+		boolean keepGoing = true;
+		while (keepGoing) {
+			System.out.println("+-------------------------------+");
+			System.out.println("|   *Please select an option*   |");
+			System.out.println("|-------------------------------|");
+			System.out.println("| 1. Look up a film by it's ID  |");
+			System.out.println("| 2. Look up a film by keyword  |");
+			System.out.println("| 3. EXIT                       |");
+			System.out.println("+-------------------------------+");
+
+			int choice = getInput();
+			input.nextLine();
+		}
+	}
+
+	private int getInput() {
+		while (true) {
+			try {
+				return input.nextInt();
+			}
+			catch (Exception e) {
+				System.out.println("Error: Input MUST be a number.");
+			}
+			input.nextLine();
+		}
+	}
 
 }
